45016db9261eb251b9342f25a6f25d5ccd33f620
souvik <sobudp73@gmail.com>
2023-05-25 03:42:29 +0530

    ok added shuffeled cards

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -15,43 +15,61 @@ function GameBoard() {
       const card2 = { id: cards.length + 2, symbol, isFlipped: false };
       return [...cards, card1, card2];
     }, []);
-    setCards(generatedCards);
+    // Shuffling cards
+    const shuffledCards = shuffleArray(generatedCards);
+    setCards(shuffledCards);
   }, []);
 
   const handleClick = (id, symbol) => {
     const flippedCount = flippedCards.length;
-    console.log("Click!!");
+    const isCardMatched = matchedCards.includes(symbol);
+  
+    if (isCardMatched) {
+      // Ignore click on matched card
+      return;
+    }
+  
     if (flippedCount < 2) {
       // Flip the clicked card
       const updatedCards = cards.map((card) =>
         card.id === id ? { ...card, isFlipped: true } : card
       );
       setCards(updatedCards);
-
+  
       // Update the flipped cards
       setFlippedCards((prevFlippedCards) => [...prevFlippedCards, { id, symbol }]);
-
+  
       // Check for a match if two cards are flipped
       if (flippedCount === 1) {
         const [card1, card2] = flippedCards;
         if (card1.symbol === symbol) {
           // Match found!
-          console.log("Match Found");
+          console.log("Match found");
           setMatchedCards((prevMatchedCards) => [...prevMatchedCards, symbol]);
           setFlippedCards([]);
         } else {
           // No match, flip the cards back after a delay
-          console.log("NOT MATCH");
+          console.log("Match not found, flipping back");
           setTimeout(() => {
             const updatedCards = cards.map((card) =>
               card.id === card1.id || card.id === id ? { ...card, isFlipped: false } : card
             );
             setCards(updatedCards);
             setFlippedCards([]);
-          }, 1000);
+          }, 500);
         }
       }
     }
+  };
+
+  // Fisher-Yates shuffle algorithm
+  const shuffleArray = (array) => {
+    const newArray = [...array];
+    for (let i = newArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+    }
+    return newArray;
   };
 
   return (
